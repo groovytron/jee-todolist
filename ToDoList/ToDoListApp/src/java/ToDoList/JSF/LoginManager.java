6f8d8f07fad96f1609ae2dd98e4f14c1137ee5c6
Julien M'Poy <julien.mpoy@gmail.com>
2017-04-24 19:25:54 +0200

    Implemented a simple logout class. It's DIY but it works.

---
+++ ToDoList/ToDoListApp/src/java/ToDoList/JSF/LoginManager.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ToDoList.JSF;
+
+import java.io.IOException;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author julien
+ */
+@Named(value = "loginManager")
+@Dependent
+public class LoginManager {
+
+    /**
+     * Creates a new instance of LoginManager
+     */
+    public LoginManager() {
+    }
+    
+    public static void logout() throws IOException {
+        HttpSession currentSession;
+        currentSession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        currentSession.invalidate();
+        HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        response.sendRedirect("login.jsp");
+    }
+    
+}
